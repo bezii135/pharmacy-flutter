a9c93aa80da6a580076582ad889461970b1252f1
beza <bzendale6@gmail.com>
2024-06-23 12:34:32 +0100

    ne updates

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {
+                //   Navigater.push(context, MaterialPageRoute
+                //   builder:(context) =>
+                //    ) ;)
+                // },
+                child: Text(
+                  "SKIP" ,
+                  style: TextStyle(
+                    color: color(0xFF7165D6),
+                    fontSize: 20,
+                  )
+                ), // Add a child to the TextButton
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
